e412bd1da0879e41916a2c3a9790cc71139c8b50
ShengHaozz <shenghaoong@gmail.com>
2026-08-24 22:39:34 +0800

    Update add command to parse datetime

--- src/main/java/commands/AddCommand.java
+++ src/main/java/commands/AddCommand.java
@@ -1,11 +1,14 @@
 package commands;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+
 import exceptions.BobException;
 import tasks.Deadline;
 import tasks.Event;
 import tasks.Task;
 import tasks.ToDo;
-
+import util.DatetimeHelper;
 
 public class AddCommand extends Command {
     public boolean processInput(String input) throws BobException {
@@ -22,44 +25,55 @@ public class AddCommand extends Command {
             switch (command) {
                 case "todo":
                     ToDo todo = new ToDo(arg);
-                    this.taskList.add(todo); 
+                    this.taskList.add(todo);
                     this.printAddition(todo);
                     break;
-                
+
                 case "deadline":
                     String[] deadlineParts = arg.split(" /by ");
                     Deadline deadline;
                     try {
-                        deadline = new Deadline(deadlineParts[0], deadlineParts[1]);
+                        deadline = new Deadline(
+                                deadlineParts[0],
+                                LocalDateTime.parse(deadlineParts[1], DatetimeHelper.INPUT_FORMATTER));
 
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        throw new BobException(
-                            """
-                            Error: No deadline set for deadline task
-                            Usage: deadline ___ \\by ___
-                            """
-                        );
-
+                        throw new BobException("""
+                                Error: No deadline set for deadline task
+                                Usage: deadline ___ \\by ___
+                                """);
+                    } catch (DateTimeParseException e) {
+                        throw new BobException("""
+                                Error: Cannot parse date
+                                Date Format: dd/MM/yy HH:mm
+                                """);
                     }
-                    this.taskList.add(deadline); 
+                    this.taskList.add(deadline);
                     this.printAddition(deadline);
                     break;
-                
+
                 case "event":
                     String[] eventParts = arg.split(" /from ", 2);
                     Event event;
                     try {
                         String[] dateParts = eventParts[1].split(" /to ", 2);
-                        event = new Event(eventParts[0], dateParts[0], dateParts[1]);
+                        event = new Event(
+                                eventParts[0],
+                                LocalDateTime.parse(dateParts[0], DatetimeHelper.INPUT_FORMATTER),
+                                LocalDateTime.parse(dateParts[1], DatetimeHelper.INPUT_FORMATTER));
                     } catch (ArrayIndexOutOfBoundsException e) {
                         throw new BobException(
-                            """
-                            Error: Missing either /from or /to
-                            Usage: event ___ /from ___ /to ___
-                            """
-                        );
+                                """
+                                        Error: Missing either /from or /to
+                                        Usage: event ___ /from ___ /to ___
+                                        """);
+                    } catch (DateTimeParseException e) {
+                        throw new BobException("""
+                                Error: Cannot parse date
+                                Date Format: dd/MM/yy HH:mm
+                                """);
                     }
-                    this.taskList.add(event); 
+                    this.taskList.add(event);
                     this.printAddition(event);
                     break;
             }
@@ -73,9 +87,8 @@ public class AddCommand extends Command {
         System.out.println("Task added:");
         System.out.println(t.toString());
         System.out.println(String.format(
-            "%d %s in list",
-            this.taskList.size(),
-            this.taskList.size() < 2 ? "item" : "items"
-        ));
+                "%d %s in list",
+                this.taskList.size(),
+                this.taskList.size() < 2 ? "item" : "items"));
     }
 }
