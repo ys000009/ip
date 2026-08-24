0e52a098a3cec31cfd5244ff72aaa10b52260bdc
ShengHaozz <shenghaoong@gmail.com>
2026-08-24 22:11:22 +0800

    Add iso formatter

--- src/main/java/util/DatetimeHelper.java
+++ src/main/java/util/DatetimeHelper.java
@@ -6,4 +6,6 @@ public class DatetimeHelper {
     public static DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
 
     public static DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("dd MMMM yyyy, HHmm 'hrs'");
+
+    public static DateTimeFormatter ISO_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 }
